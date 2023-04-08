2c83581c1fa91b385d42527d9837e3d63465fb2a
cc031023 <1041526008@qq.com>
2023-04-08 23:42:06 +0800

    难度:简单

---
+++ 2023.04.08LeetCode_0009.cpp
@@ -0,0 +1,11 @@
+class Solution
+{
+public:
+    bool isPalindrome(int x)
+    {
+        string s = to_string(x);
+        string s2 = s;
+        reverse(s.begin(), s.end());
+        return s2 == s;
+    }
+};
