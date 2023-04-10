cfa60a429ff4be2e48b67929f4a061a304414a07
cc031023 <1041526008@qq.com>
2023-04-10 23:44:00 +0800

    难度:简单

---
+++ 2023.04.10LeetCode_0020.cpp
@@ -0,0 +1,45 @@
+class Solution
+{
+public:
+    bool isValid(string s)
+    {
+        stack<char> st;
+        int len = s.size();
+        for (int i = 0; i < len; ++i)
+        {
+            if (s[i] == '(' || s[i] == '[' || s[i] == '{')
+            {
+                st.push(s[i]);
+            }
+            else
+            {
+                if (st.empty())
+                {
+                    return false;
+                }
+                if (s[i] == ')')
+                {
+                    if (st.top() == '(')
+                        st.pop();
+                    else
+                        return false;
+                }
+                else if (s[i] == ']')
+                {
+                    if (st.top() == '[')
+                        st.pop();
+                    else
+                        return false;
+                }
+                else
+                {
+                    if (st.top() == '{')
+                        st.pop();
+                    else
+                        return false;
+                }
+            }
+        }
+        return st.empty();
+    }
+};
