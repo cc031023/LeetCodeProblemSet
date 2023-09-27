2ae7e97d33b5ae1f2563e0b1c8715a6587b1e7fb
cc031023 <1041526008@qq.com>
2023-09-27 22:43:58 +0800

    难度:简单(最近公共祖先)

---
+++ 2023.09.27LeetCode_LCR 193.cpp
@@ -0,0 +1,33 @@
+
+class Solution {
+private:
+    static vector<TreeNode*> GetPath(TreeNode* root, TreeNode* cur) {
+        vector<TreeNode*> path;
+
+        while (root != cur) {
+            path.emplace_back(root);
+            if (root->val > cur->val)
+                root = root->left;
+            else
+                root = root->right;
+        }
+
+        path.emplace_back(root);
+
+        return path;
+    }
+
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        vector<TreeNode*> path1 = GetPath(root, p);
+        vector<TreeNode*> path2 = GetPath(root, q);
+        TreeNode* ans = nullptr;
+
+        for (int i = 0; i < path1.size() && i < path2.size(); ++i) {
+            if (path1[i] != path2[i]) return ans;
+            ans = path1[i];
+        }
+
+        return ans;
+    }
+};
