0a0d79aec247b7d0d6f8738f85ca8017f8eabb94
cc031023 <1041526008@qq.com>
2023-10-01 15:02:31 +0800

    难度:困难

--- 2023.10.01LeetCode_LCR156.cpp
+++ 2023.10.01LeetCode_LCR156.cpp
@@ -1,48 +1,62 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
 class Codec {
 private:
-    TreeNode* Ldfs(list<string>& datalist) {
-        if (datalist.front() == "nullptr") {
-            datalist.erase(datalist.begin());
+    void GetString(TreeNode* root, string& s) {
+        if (!root) {
+            s += "nullptr,";
+        } else {
+            s += to_string(root->val) + ',';
+            GetString(root->left, s);
+            GetString(root->right, s);
+        }
+    }
+
+    TreeNode* GetRoot(list<string>& slt) {
+        if (slt.front() == "nullptr") {
+            slt.erase(slt.begin());
             return nullptr;
         }
 
-        TreeNode* root = new TreeNode(stoi(datalist.front()));
-        datalist.erase(datalist.begin());
-        root->left = Ldfs(datalist);
-        root->right = Ldfs(datalist);
-        return root;
-    }
+        TreeNode* root = new TreeNode(stoi(slt.front()));
+        slt.erase(slt.begin());
+        root->left = GetRoot(slt);
+        root->right = GetRoot(slt);
 
-    void Sdfs(TreeNode* root, string& ans) {
-        if (!root) {
-            ans += "nullptr,";
-        } else {
-            ans += to_string(root->val) + ',';
-            Sdfs(root->left, ans);
-            Sdfs(root->right, ans);
-        }
+        return root;
     }
 
 public:
     // Encodes a tree to a single string.
     string serialize(TreeNode* root) {
         string ans;
-        Sdfs(root, ans);
+        GetString(root, ans);
         return ans;
     }
 
     // Decodes your encoded data to tree.
     TreeNode* deserialize(string data) {
-        list<string> datalist;
-        string str;
+        list<string> slist;
+        string now;
         for (auto& ch : data) {
             if (ch == ',') {
-                datalist.push_back(str);
-                str.clear();
+                slist.push_back(now);
+                now.clear();
             } else {
-                str += ch;
+                now += ch;
             }
         }
-        return Ldfs(datalist);
+        return GetRoot(slist);
     }
 };
+
+// Your Codec object will be instantiated and called as such:
+// Codec ser, deser;
+// TreeNode* ans = deser.deserialize(ser.serialize(root));
