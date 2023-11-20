775b2873fa8eb462cffa97b04dc497447d615a5f
cc031023 <1041526008@qq.com>
2023-11-20 12:12:25 +0800

    难度:中等(并查集)

---
+++ 2023.11.20LeetCode_0684.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+        int n = edges.size();
+        vector<int> f(n + 1);
+        init(n, f);
+        for (const auto& e : edges) {
+            if (find(e[0], f) != find(e[1], f))
+                merge(e[0], e[1], f);
+            else
+                return e;
+        }
+        return {};
+    }
+
+private:
+    void init(int n, vector<int>& f) {
+        for (int i = 1; i <= n; ++i)
+            f[i] = i;
+    }
+
+    int find(int x, vector<int>& f) {
+        return x == f[x] ? x : f[x] = find(f[x], f);
+    }
+
+    void merge(int x, int y, vector<int>& f) {
+        int fx = find(x, f);
+        int fy = find(y, f);
+        if (fx != fy) { f[fy] = fx; }
+    }
+};
